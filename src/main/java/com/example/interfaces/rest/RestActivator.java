1da37bb927002739bd79cf86d6da089a12d0f593
hantsy <hantsy@gmail.com>
2026-08-25 13:51:54 +0800

    chore: clean up

--- src/main/java/com/example/interfaces/rest/RestActivator.java
+++ src/main/java/com/example/interfaces/rest/RestActivator.java
@@ -8,5 +8,6 @@ import org.eclipse.microprofile.auth.LoginConfig;
 
 @ApplicationPath("/api")
 @ApplicationScoped
+@LoginConfig(authMethod = "MP-JWT", realmName = "MP JWT Realm")
 public class RestActivator extends Application {
 }
